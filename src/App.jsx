954102efdd68c7e556117db2ab57f7d940bdb0f8
Sravan Surya <vsravansurya@gmail.com>
2025-03-18 20:17:52 +0530

    tailwind + daisyUI + navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import NavBar from "./components/NavBar"
 
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
+    <NavBar />
+      <div className='text-3xl font-bold underline'>
        Hello World
       </div>
       
