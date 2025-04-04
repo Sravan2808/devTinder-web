870c18dab57b4f9563a2a764ec04dad681a84fe8
Sravan Surya <vsravansurya@gmail.com>
2025-04-04 13:42:03 +0530

    DevTinder---->DevMate

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
       <div className="bg-gradient-to-r from-blue-500 to-purple-600 p-2 rounded-full">
         <span className="text-white text-2xl font-bold">🧑‍💻</span>
       </div>
-      <span className="text-xl font-semibold text-gray-800">DevTinder</span>
+      <span className="text-xl font-semibold text-gray-800">DevMate</span>
     </div>
   );
 };
